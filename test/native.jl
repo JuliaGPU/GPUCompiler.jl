7e4ab3ee05cc9cf0dfa3d9175dd853d6e23270f5
Julian P Samaroo <jpsamaroo@jpsamaroo.me>
2020-12-14 15:56:49 -0600

    Fix flaky native test

--- test/native.jl
+++ test/native.jl
@@ -251,8 +251,10 @@ end
 
     @test_throws_message(InvalidIRError,
                          native_code_execution(foobar, Tuple{Int})) do msg
+        println(msg)
         occursin("invalid LLVM IR", msg) &&
-        occursin(GPUCompiler.RUNTIME_FUNCTION, msg) &&
+        (occursin(GPUCompiler.RUNTIME_FUNCTION, msg) ||
+         occursin(GPUCompiler.UNKNOWN_FUNCTION, msg)) &&
         occursin("[1] println", msg) &&
         occursin(r"\[2\] .+foobar", msg)
     end
