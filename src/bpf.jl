4d6924dcd3db908fce74e528808e464eded30c86
Julian P Samaroo <jpsamaroo@jpsamaroo.me>
2021-04-29 15:52:11 -0500

    Don't do finish_module in BPF backend

--- src/bpf.jl
+++ src/bpf.jl
@@ -5,8 +5,6 @@
 export BPFCompilerTarget
 
 Base.@kwdef struct BPFCompilerTarget <: AbstractCompilerTarget
-    prog_section::String="prog" # section for kernel to be placed in
-    license::String="" # license for kernel and source code
     function_pointers::UnitRange{Int}=1:1000 # set of valid function "pointers"
 end
 
@@ -32,33 +30,6 @@ runtime_slug(job::CompilerJob{BPFCompilerTarget}) = "bpf"
 
 const bpf_intrinsics = () # TODO
 isintrinsic(::CompilerJob{BPFCompilerTarget}, fn::String) = in(fn, bpf_intrinsics)
-
-function finish_module!(job::CompilerJob{BPFCompilerTarget}, mod::LLVM.Module)
-    for func in LLVM.functions(mod)
-        if LLVM.name(func) == "gpu_signal_exception"
-            throw(KernelError(job, "eBPF does not support exceptions"))
-        end
-        # Set entry section for loaders like libbpf
-        LLVM.section!(func, job.target.prog_section)
-    end
-
-    # Set license
-    license = job.target.license
-    if license != ""
-        ctx = LLVM.context(mod)
-        i8 = LLVM.Int8Type(ctx)
-        glob = GlobalVariable(mod, LLVM.ArrayType(i8, length(license)+1), "_license")
-        linkage!(glob, LLVM.API.LLVMExternalLinkage)
-        constant!(glob, true)
-        section!(glob, "license")
-        initializer!(glob, ConstantArray(i8, vcat(Vector{UInt8}(license),0x0)))
-    end
-
-    # Set all map definitions as external linkage
-    for gv in filter(x->(section(x)=="maps")||(section(x)==".maps"), collect(LLVM.globals(mod)))
-        linkage!(gv, LLVM.API.LLVMExternalLinkage)
-    end
-end
 
 valid_function_pointer(job::CompilerJob{BPFCompilerTarget}, ptr::Ptr{Cvoid}) =
     reinterpret(UInt, ptr) in job.target.function_pointers
