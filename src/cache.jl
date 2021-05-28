477378bf803eeb2fa5e8761cfce89205eaff0470
Tim Besard <tim.besard@gmail.com>
2021-05-28 14:13:12 +0200

    Simplify the generator for tracking function redefinitions.

--- src/cache.jl
+++ src/cache.jl
@@ -3,13 +3,71 @@
 using Core.Compiler: retrieve_code_info, CodeInfo, MethodInstance, SSAValue, SlotNumber, ReturnNode
 using Base: _methods_by_ftype
 
-using Serialization, Scratch
+# generated function that crafts a custom code info to call the actual compiler.
+# this gives us the flexibility to insert manual back edges for automatic recompilation.
+#
+# we also increment a global specialization counter and pass it along to index the cache.
+
+const specialization_counter = Ref{UInt}(0)
+@generated function specialization_id(job::CompilerJob{<:Any,<:Any,FunctionSpec{f,tt}}) where {f,tt}
+    # get a hold of the method and code info of the kernel function
+    sig = Tuple{f, tt.parameters...}
+    # XXX: instead of typemax(UInt) we should use the world-age of the fspec
+    mthds = _methods_by_ftype(sig, -1, typemax(UInt))
+    Base.isdispatchtuple(tt) || return(:(error("$tt is not a dispatch tuple")))
+    length(mthds) == 1 || return (:(throw(MethodError(job.source.f,job.source.tt))))
+    mtypes, msp, m = mthds[1]
+    mi = ccall(:jl_specializations_get_linfo, Ref{MethodInstance}, (Any, Any, Any), m, mtypes, msp)
+    ci = retrieve_code_info(mi)
+    @assert isa(ci, CodeInfo)
+
+    # generate a unique id to represent this specialization
+    # TODO: should we make this the world age of the method instance?
+    id = (specialization_counter[] += 1)
+    # TODO: save the mi/ci here (or embed it in the AST to pass to the compiler)
+    #       and use that to drive compilation
+
+    # prepare a new code info
+    new_ci = copy(ci)
+    empty!(new_ci.code)
+    empty!(new_ci.codelocs)
+    resize!(new_ci.linetable, 1)                # see note below
+    empty!(new_ci.ssaflags)
+    new_ci.ssavaluetypes = 0
+    new_ci.edges = MethodInstance[mi]
+    # XXX: setting this edge does not give us proper method invalidation, see
+    #      JuliaLang/julia#34962 which demonstrates we also need to "call" the kernel.
+    #      invoking `code_llvm` also does the necessary codegen, as does calling the
+    #      underlying C methods -- which GPUCompiler does, so everything Just Works.
+
+    # prepare the slots
+    new_ci.slotnames = Symbol[Symbol("#self#"), :cache, :job, :compiler, :linker]
+    new_ci.slotflags = UInt8[0x00 for i = 1:5]
+    cache = SlotNumber(2)
+    job = SlotNumber(3)
+    compiler = SlotNumber(4)
+    linker = SlotNumber(5)
+
+    # call the compiler
+    append!(new_ci.code, [ReturnNode(id)])
+    append!(new_ci.codelocs, [1])   # see note below
+    new_ci.ssavaluetypes += 1
+
+    # NOTE: we keep the first entry of the original linetable, and use it for location info
+    #       on the call to check_cache. we can't not have a codeloc (using 0 causes
+    #       corruption of the back trace), and reusing the target function's info
+    #       has as advantage that we see the name of the kernel in the backtraces.
+
+    return new_ci
+end
 
 const cache_lock = ReentrantLock()
-
-@inline function check_cache(cache, job, prekey,
-                             @nospecialize(compiler), @nospecialize(linker))
-    key = hash(job, prekey)
+function cached_compilation(cache::AbstractDict,
+                            @nospecialize(job::CompilerJob),
+                            compiler::Function, linker::Function)
+    # XXX: CompilerJob contains a world age, so can't be respecialized.
+    #      have specialization_id take a f/tt and return a world to construct a CompilerJob?
+    key = hash(job, specialization_id(job))
     force_compilation = compile_hook[] !== nothing
 
     # XXX: by taking the hash, we index the compilation cache directly with the world age.
@@ -41,65 +99,4 @@ const cache_lock = ReentrantLock()
     finally
         unlock(cache_lock)
     end
-end
-
-# generated function that crafts a custom code info to call the actual compiler.
-# this gives us the flexibility to insert manual back edges for automatic recompilation.
-#
-# we also increment a global specialization counter and pass it along to index the cache.
-
-const specialization_counter = Ref{UInt}(0)
-
-@generated function cached_compilation(cache::AbstractDict,
-                                       job::CompilerJob{<:Any,<:Any,FunctionSpec{f,tt}},
-                                       compiler::Function, linker::Function) where {f,tt}
-    # get a hold of the method and code info of the kernel function
-    sig = Tuple{f, tt.parameters...}
-    # XXX: instead of typemax(UInt) we should use the world-age of the fspec
-    mthds = _methods_by_ftype(sig, -1, typemax(UInt))
-    Base.isdispatchtuple(tt) || return(:(error("$tt is not a dispatch tuple")))
-    length(mthds) == 1 || return (:(throw(MethodError(job.source.f,job.source.tt))))
-    mtypes, msp, m = mthds[1]
-    mi = ccall(:jl_specializations_get_linfo, Ref{MethodInstance}, (Any, Any, Any), m, mtypes, msp)
-    ci = retrieve_code_info(mi)
-    @assert isa(ci, CodeInfo)
-
-    # generate a unique id to represent this specialization
-    id = (specialization_counter[] += 1)
-    # TODO: save the mi/ci here (or embed it in the AST to pass to the compiler)
-    #       and use that to drive compilation
-
-    # prepare a new code info
-    new_ci = copy(ci)
-    empty!(new_ci.code)
-    empty!(new_ci.codelocs)
-    resize!(new_ci.linetable, 1)                # see note below
-    empty!(new_ci.ssaflags)
-    new_ci.ssavaluetypes = 0
-    new_ci.edges = MethodInstance[mi]
-    # XXX: setting this edge does not give us proper method invalidation, see
-    #      JuliaLang/julia#34962 which demonstrates we also need to "call" the kernel.
-    #      invoking `code_llvm` also does the necessary codegen, as does calling the
-    #      underlying C methods -- which GPUCompiler does, so everything Just Works.
-
-    # prepare the slots
-    new_ci.slotnames = Symbol[Symbol("#self#"), :cache, :job, :compiler, :linker]
-    new_ci.slotflags = UInt8[0x00 for i = 1:5]
-    cache = SlotNumber(2)
-    job = SlotNumber(3)
-    compiler = SlotNumber(4)
-    linker = SlotNumber(5)
-
-    # call the compiler
-    append!(new_ci.code, [Expr(:call, check_cache, cache, job, id, compiler, linker),
-                          ReturnNode(SSAValue(1))])
-    append!(new_ci.codelocs, [1, 1])   # see note below
-    new_ci.ssavaluetypes += 2
-
-    # NOTE: we keep the first entry of the original linetable, and use it for location info
-    #       on the call to check_cache. we can't not have a codeloc (using 0 causes
-    #       corruption of the back trace), and reusing the target function's info
-    #       has as advantage that we see the name of the kernel in the backtraces.
-
-    return new_ci
 end
