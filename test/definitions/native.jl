aed26ec9caf49b16b85d3bbd653deb085c47563f
Tim Besard <tim.besard@gmail.com>
2021-06-23 11:30:45 +0200

    Use assume from LLVM.jl.

--- test/definitions/native.jl
+++ test/definitions/native.jl
@@ -51,6 +51,7 @@ end
 
 module LazyCodegen
     using LLVM
+    using LLVM.Interop
     using GPUCompiler
 
     import ..native_job
@@ -105,18 +106,6 @@ module LazyCodegen
         # 5. Return the address of the implementation, since we are going to call it now
         return UInt64(reinterpret(UInt, ptr))
     end
-
-    @inline assume(cond::Bool) = Base.llvmcall(("""
-    declare void @llvm.assume(i1)
-
-    define void @entry(i8) #0 {
-        %cond = icmp eq i8 %0, 1
-        call void @llvm.assume(i1 %cond)
-        ret void
-    }
-
-    attributes #0 = { alwaysinline }""", "entry"),
-    Nothing, Tuple{Bool}, cond)
 
 
     @generated function deferred_codegen(::Val{f}, ::Val{tt}) where {f,tt}
