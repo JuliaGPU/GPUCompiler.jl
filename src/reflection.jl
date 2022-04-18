31f2573ce8e7e902893aaa9c415a6d898f921dc4
William S. Moses <gh@wsmoses.com>
2022-04-18 01:36:58 -0400

    Fix 1.6

--- src/reflection.jl
+++ src/reflection.jl
@@ -58,13 +58,13 @@ function code_typed(@nospecialize(job::CompilerJob); interactive::Bool=false, kw
         descend_code_typed(job.source.f, job.source.tt; interp, kwargs...)
     elseif VERSION >= v"1.7-"
         interp = get_interpreter(job)
-        # InteractiveUtils.code_typed(job.source.f, job.source.tt; interp, kwargs...)
-        # tt = Tuple{job.source.f, job.source.tt...}
         u = Base.unwrap_unionall(job.source.tt)
         tt = Base.rewrap_unionall(Tuple{job.source.f, u.parameters...}, job.source.tt)
         Base.code_typed_by_type(tt; interp, kwargs...)
     else
-        InteractiveUtils.code_typed(job.source.f, job.source.tt; kwargs...)
+        u = Base.unwrap_unionall(job.source.tt)
+        tt = Base.rewrap_unionall(Tuple{job.source.f, u.parameters...}, job.source.tt)
+        Base.code_typed_by_type(tt; kwargs...)
     end
 end
 
