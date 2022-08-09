c79739f8ad7b5e4a6171e0a07ba584aeb1e740d4
Tim Besard <tim.besard@gmail.com>
2022-08-09 14:10:09 +0200

    Use Base.active_project. (#352)

--- test/examples.jl
+++ test/examples.jl
@@ -19,11 +19,7 @@ filter!(file -> !occursin("Kaleidoscope", file), examples)
 cd(examples_dir) do
     examples = relpath.(examples, Ref(examples_dir))
     @testset for example in examples
-        cmd = Base.julia_cmd()
-        if Base.JLOptions().project != C_NULL
-            cmd = `$cmd --project=$(unsafe_string(Base.JLOptions().project))`
-        end
-
+        cmd = `$(Base.julia_cmd()) --project=$(Base.active_project())`
         @test success(pipeline(`$cmd $example`, stderr=stderr))
     end
 end
