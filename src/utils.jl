7a45af6ee106d2d44976e96b117a9c23bf038e56
Tim Besard <tim.besard@gmail.com>
2023-04-19 11:55:12 +0200

    Prevent invalidations of safe logging functionality by custom loggers. (#432)

--- src/utils.jl
+++ src/utils.jl
@@ -43,6 +43,12 @@ using Logging
 
 const STDERR_HAS_COLOR = Ref{Bool}(false)
 
+# Prevent invalidation when packages define custom loggers
+# Using invoke in combination with @nospecialize eliminates backedges to these methods
+function _invoked_min_enabled_level(@nospecialize(logger))
+    return invoke(Logging.min_enabled_level, Tuple{typeof(logger)}, logger)::LogLevel
+end
+
 # define safe loggers for use in generated functions (where task switches are not allowed)
 for level in [:debug, :info, :warn, :error]
     @eval begin
@@ -53,7 +59,7 @@ for level in [:debug, :info, :warn, :error]
             quote
                 old_logger = global_logger()
                 io = IOContext(Core.stderr, :color=>STDERR_HAS_COLOR[])
-                min_level = Logging.min_enabled_level(old_logger)
+                min_level = _invoked_min_enabled_level(old_logger)
                 global_logger(Logging.ConsoleLogger(io, min_level))
                 ret = $(esc(macrocall))
                 global_logger(old_logger)
