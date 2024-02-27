3146658e94d792994d31ff5c2a721a30de933558
Tim Besard <tim.besard@gmail.com>
2024-02-27 16:16:02 +0100

    Add CI for 1.11. (#551)

--- .github/download_build.sh
+++ .github/download_build.sh
@@ -1,17 +1,18 @@
 #!/bin/bash -e
 
 # handle user inputs
-[ $# -ne 2 ] && { echo "Usage: $0 <build_name> <destination_file>" >&2; exit 1; }
-BUILD_NAME="$1"
-DEST_FILE="$2"
+[ $# -ne 4 ] && { echo "Usage: $0 <pipeline> <branch> <build_name> <destination_file>" >&2; exit 1; }
+PIPELINE="$1"
+BRANCH="$2"
+BUILD_NAME="$3"
+DEST_FILE="$4"
 [ -z "$BUILDKITE_TOKEN" ] && { echo "BUILDKITE_TOKEN not set." >&2; exit 1; }
 
 API_BASE="https://api.buildkite.com/v2"
 ORG="julialang"
-PIPELINE="julia-master"
 
-# find the first successful build on the master branch, and get its artifacts url
-ARTIFACTS_URL=$(curl -s -H "Authorization: Bearer $BUILDKITE_TOKEN" "$API_BASE/organizations/$ORG/pipelines/$PIPELINE/builds?branch=master" | \
+# find the first successful build on the requested branch, and get its artifacts url
+ARTIFACTS_URL=$(curl -s -H "Authorization: Bearer $BUILDKITE_TOKEN" "$API_BASE/organizations/$ORG/pipelines/$PIPELINE/builds?branch=$BRANCH" | \
     jq -r "first(.[] | .jobs[] | select(.step_key == \"$BUILD_NAME\" and .exit_status == 0) | .artifacts_url)")
 [ -z "$ARTIFACTS_URL" ] && { echo "No successful build found."; exit 1; }
 
