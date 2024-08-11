0c22da53fff41538c03defdd74e87a8c06694314
Simon Byrne <simonbyrne@gmail.com>
2024-08-10 22:14:13 -0700

    fix mangling of booleans and zeros

--- test/util_tests.jl
+++ test/util_tests.jl
@@ -9,4 +9,14 @@
     @test groups[3] == [:(d=4)]
 end
 
+@testset "mangle" begin
+    struct XX{T} end
+    # values checked with c++filt / cu++filt
+    @test GPUCompiler.mangle_sig(Tuple{typeof(sin), XX{false}})      == "_Z3sin2XXILb0EE"    # "sin(XX<false>)"
+    @test GPUCompiler.mangle_sig(Tuple{typeof(sin), XX{true}})       == "_Z3sin2XXILb1EE"    # "sin(XX<true>)"
+    @test GPUCompiler.mangle_sig(Tuple{typeof(sin), XX{Int64(10)}})  == "_Z3sin2XXILl10EE"   # "sin(XX<10l>)"
+    @test GPUCompiler.mangle_sig(Tuple{typeof(sin), XX{Int64(0)}})   == "_Z3sin2XXILl0EE"    # "sin(XX<0l>)"
+    @test GPUCompiler.mangle_sig(Tuple{typeof(sin), XX{Int64(-10)}}) == "_Z3sin2XXILln10EE"  # "sin(XX<-10l>)"
+end
+
 end
