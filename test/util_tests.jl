df2c8887e2df4741a9f0003052d7ac8dbd9f1a9b
Daniel Wennberg <daniel.wennberg@gmail.com>
2024-08-27 09:18:18 -0700

    Add tests for safe loggers

--- test/util_tests.jl
+++ test/util_tests.jl
@@ -19,4 +19,49 @@ end
     @test GPUCompiler.mangle_sig(Tuple{typeof(sin), XX{Int64(-10)}}) == "_Z3sin2XXILln10EE"  # "sin(XX<-10l>)"
 end
 
+@testset "safe loggers" begin
+    using Logging: Logging
+
+    struct YieldingLogger <: Logging.AbstractLogger
+        logger::Logging.AbstractLogger
+        YieldingLogger() = new(Logging.current_logger())
+    end
+
+    function Logging.handle_message(logger::YieldingLogger, args...)
+        yield()
+        return Logging.handle_message(logger.logger, args...)
+    end
+
+    Logging.shouldlog(::YieldingLogger, ::Any...) = true
+    Logging.min_enabled_level(::YieldingLogger) = Logging.Debug
+
+    GPUCompiler.@locked function f()
+        GPUCompiler.@safe_debug "safe_debug"
+        GPUCompiler.@safe_info "safe_info"
+        GPUCompiler.@safe_warn "safe_warn"
+        GPUCompiler.@safe_error "safe_error"
+    end
+
+    @test begin
+        @sync begin
+            Threads.@spawn begin
+                sleep(0.1)
+                @debug "debug"
+                sleep(0.1)
+                @info "info"
+                sleep(0.1)
+                @warn "warn"
+                sleep(0.1)
+                @error "error"
+                sleep(0.1)
+            end
+            Threads.@spawn Logging.with_logger(YieldingLogger()) do
+                sleep(0.1)
+                f()
+            end
+        end
+        true
+    end
+end
+
 end
