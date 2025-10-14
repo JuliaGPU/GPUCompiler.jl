523cbfcd2126b6b48dc804b12151d0c051c75310
Tim Besard <tim.besard@gmail.com>
2025-10-14 11:54:24 +0200

    Bump ParallelTestRunner.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,8 @@
-using ParallelTestRunner 
-import LLVM
+using ParallelTestRunner
+import GPUCompiler, LLVM
 
 const init_code = quote
-    using Test, GPUCompiler, LLVM
+    using GPUCompiler, LLVM
     using SPIRV_LLVM_Backend_jll, SPIRV_LLVM_Translator_jll, SPIRV_Tools_jll
 
     # include all helpers
@@ -15,18 +15,18 @@ const init_code = quote
     using .FileCheck
 end
 
-function testfilter(test)
+function test_filter(test)
     if startswith(test, "helpers/")
         return false
     end
-    if LLVM.is_asserts() && test == "gcn" 
+    if LLVM.is_asserts() && test == "gcn"
         # XXX: GCN's non-0 stack address space triggers LLVM assertions due to Julia bugs
         return false
      end
      if VERSION < v"1.11" && test in ("ptx/precompile", "native/precompile")
-         return false   
+         return false
      end
     return true
 end
 
-runtests(ARGS; init_code, testfilter)
+runtests(GPUCompiler, ARGS; init_code, test_filter)
