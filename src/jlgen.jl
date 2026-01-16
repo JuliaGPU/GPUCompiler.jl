1bd4f59049fe6c9ee3af0239d978de9ca12cb756
William S. Moses <gh@wsmoses.com>
2026-01-16 12:42:31 -0600

    Fix codeinstance mapping when using nested interpretation

--- src/jlgen.jl
+++ src/jlgen.jl
@@ -670,6 +670,11 @@ function Base.precompile(@nospecialize(job::CompilerJob))
     return true
 end
 
+
+const HAS_LLVM_GET_CIS = VERSION >= v"1.13.0-DEV.1120" || Libdl.dlsym(
+    unsafe_load(cglobal(:jl_libjulia_handle, Ptr{Cvoid})), :jl_get_llvm_cis, throw_error = false
+) !== nothing
+
 function compile_method_instance(@nospecialize(job::CompilerJob))
     if job.source.def.primary_world > job.world
         error("Cannot compile $(job.source) for world $(job.world); method is only valid from world $(job.source.def.primary_world) onwards")
@@ -786,7 +791,9 @@ function compile_method_instance(@nospecialize(job::CompilerJob))
         end
     end
 
-    if VERSION >= v"1.13.0-DEV.1120"
+    code_instances = Core.CodeInstance[]
+
+    if HAS_LLVM_GET_CIS
         # on sufficiently recent versions of Julia, we can query the CIs compiled.
         # this is required after the move to `invoke(::CodeInstance)`, because our
         # lookup function (used to populate method_instances) isn't always called then.
@@ -794,14 +801,10 @@ function compile_method_instance(@nospecialize(job::CompilerJob))
         num_cis = Ref{Csize_t}(0)
         @ccall jl_get_llvm_cis(native_code::Ptr{Cvoid}, num_cis::Ptr{Csize_t},
                                C_NULL::Ptr{Cvoid})::Nothing
-        resize!(method_instances, num_cis[])
+        resize!(code_instances, num_cis[])
         @ccall jl_get_llvm_cis(native_code::Ptr{Cvoid}, num_cis::Ptr{Csize_t},
-                               method_instances::Ptr{Cvoid})::Nothing
-
-        for (i, ci) in enumerate(method_instances)
-            method_instances[i] = ci.def::MethodInstance
-        end
-
+            code_instances::Ptr{Cvoid}
+        )::Nothing
     elseif VERSION >= v"1.12.0-DEV.1703"
         # slightly older versions of Julia used MIs directly
 
@@ -813,11 +816,31 @@ function compile_method_instance(@nospecialize(job::CompilerJob))
                                method_instances::Ptr{Cvoid})::Nothing
     end
 
-    # process all compiled method instances
-    compiled = Dict()
-    for mi in method_instances
+    if !HAS_LLVM_GET_CIS
         ci = ci_cache_lookup(cache, mi, job.world, job.world)
         ci === nothing && continue
+
+        llvm_func_idx = Ref{Int32}(-1)
+        llvm_specfunc_idx = Ref{Int32}(-1)
+        ccall(
+            :jl_get_function_id, Nothing,
+            (Ptr{Cvoid}, Any, Ptr{Int32}, Ptr{Int32}),
+            native_code, ci, llvm_func_idx, llvm_specfunc_idx
+        )
+        if llvm_func_idx[] != -1
+            continue
+        end
+        push!(code_instances, ci)
+    end
+
+    resize!(method_instances, length(code_instances))
+    for (i, ci) in enumerate(code_instances)
+        code_instances[i] = ci.def::MethodInstance
+    end
+
+    # process all compiled method instances
+    compiled = Dict()
+    for (ci, mi) in zip(code_instances, method_instances)
 
         # get the function index
         llvm_func_idx = Ref{Int32}(-1)
@@ -845,6 +868,8 @@ function compile_method_instance(@nospecialize(job::CompilerJob))
         else
             nothing
         end
+
+        @assert !haskey(compiled, mi)
 
         # NOTE: it's not safe to store raw LLVM functions here, since those may get
         #       removed or renamed during optimization, so we store their name instead.
