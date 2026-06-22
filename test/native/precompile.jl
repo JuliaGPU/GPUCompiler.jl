68883fe592afc6d9520c6f9b43a8868b490de244
Tim Besard <tim.besard@gmail.com>
2026-06-22 15:18:37 +0200

    TTFX-related improvements (#856)

--- test/native/precompile.jl
+++ test/native/precompile.jl
@@ -25,6 +25,10 @@ precompile_test_harness("Inference caching") do load_path
             return x*x
         end
 
+        function checked_convert(x)
+            return Int(x)
+        end
+
         let
             job, _ = NativeCompiler.Native.create_job(kernel, (Vector{Int}, Int))
             precompile(job)
@@ -44,6 +48,13 @@ precompile_test_harness("Inference caching") do load_path
         # identity is foreign
         @setup_workload begin
             job, _ = NativeCompiler.Native.create_job(identity, (Int,))
+            @compile_workload begin
+                precompile(job)
+            end
+        end
+
+        @setup_workload begin
+            job, _ = NativeCompiler.Native.create_job(checked_convert, (UInt,); validate=false)
             @compile_workload begin
                 precompile(job)
             end
@@ -77,6 +88,21 @@ precompile_test_harness("Inference caching") do load_path
 
         # check that identity survived
         @test check_presence(identity_mi, token) broken=(v"1.12.0-DEV.1268" <= VERSION < v"1.12.5" || v"1.13.0-" <= VERSION < v"1.13.0-beta3"|| v"1.14.0-" <= VERSION < v"1.14.0-DEV.1843")
+
+        # Recompiling a foreign method after loading precompiled owner-token CIs
+        # may also surface a native owner-less CI for the same MethodInstance.
+        # GPUCompiler should prefer the owner-token CI instead of recording both.
+        job, _ = NativeCompiler.Native.create_job(identity, (Int,))
+        JuliaContext() do ctx
+            _, meta = GPUCompiler.compile(:llvm, job)
+            @test haskey(meta.compiled, job.source)
+        end
+
+        job, _ = NativeCompiler.Native.create_job(NativeBackend.checked_convert, (UInt,); validate=false)
+        JuliaContext() do ctx
+            _, meta = GPUCompiler.compile(:llvm, job)
+            @test haskey(meta.compiled, job.source)
+        end
 
         GPUCompiler.clear_disk_cache!()
         @test GPUCompiler.disk_cache_enabled() == false
